0ffa655619a662f045b1236fea77226caf41c3a4
Yusuf YILDIRIM <yusuf.yildirim@softtech.com.tr>
2021-02-22 15:45:38 +0300

    Info Comp

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
-  <div class="container">
-    <Data/>
+  <div class="info-header">
+    <Info
+        :Header="Header"
+        :Text="Text"
+        :Style="Style"
+        :Icon="Icon"
+        :Border="Border"
+        :FontType="FontType"
+    />
   </div>
 </template>
 
 <script>
-import Data from './components/Data.vue'
+import Info from './components/Info.vue';
+import Data from './components/Data.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
+    Info,
     Data
+  },
+  data() {
+    return {
+      Header: "",
+      Text: "Info Text",
+      Style: "green-info",
+      Icon: "",
+      Border: true
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Info.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="info" :class="Style">
+    <div class="info-icon" v-if="Icon">
+      <img alt="info-img" :src="getImgUrl(pic)" style="width: 30px;">
+    </div>
+    <div class="info-content">
+      <h1 class="info-header" v-if="Header">{{ Header }}</h1>
+      <h3 class="info-text" v-if="Text">{{ Text }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: [
+    'Header',
+    'Text',
+    'Style',
+    'Border',
+    'FontType',
+    'Icon'
+  ],
+  methods:{
+    getImgUrl() {
+    var images = require.context('../assets/', false, /\.png$/)
+    return images('./' + this.Icon + ".png")
+    },
+    created() {
+    let ckeditor = document.createElement('script');
+    ckeditor.setAttribute('src',"//cdn.ckeditor.com/4.6.2/full/ckeditor.js");
+    document.head.appendChild(ckeditor);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.info {
+  width: 40%;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-left: 30px;
+  margin-left: 50px ;
+  border-radius: 25px;
+  margin-top: 50px;
+
+}
+.info-icon {
+  width: 20%;
+}
+.info-content {
+  width: 80%;
+}
+.info-content .info-header {
+  color:black;
+}
+.info-content .info-text {
+  color:black;
+}
+.info.blue-info {
+  background: lightskyblue;
+  border: black;
+  color: blue;
+}
+.info.green-info {
+  background: palegreen;
+  border: black;
+  color: green;
+}
+.info.yellow-info {
+  background: rgb(199, 199, 92);
+  border: black;
+  color: black;
+}
+.info.red-info {
+  background: rgb(224, 107, 107);
+  border: black;
+  color: red;
+}
+</style>
